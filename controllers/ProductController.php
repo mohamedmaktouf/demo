c8049ae4816cd65d6bbde7c2c53d56a397b5a81d
mouhamed maktouf <mohamedmaktouf25@gmail.com>
2022-08-20 19:26:05 +0100

    enable cors

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
 class ProductController extends ActiveController
@@ -11,6 +13,34 @@ class ProductController extends ActiveController
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
+    private $_verbs = ['GET','POST','PATCH','PUT','DELETE'];
+
+    /**
+     * @return array|array[]
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // remove auth filter
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => Cors::class,
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => $this->_verbs,
+                'Access-Control-Allow-Headers' => ['content-type'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+        
+
+        return $behaviors;
+    }
+
+
     public function actionIndex()
     {
         return $this->render('index');
